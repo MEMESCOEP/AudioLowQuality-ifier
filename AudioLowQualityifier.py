899ce41e5ec223b1b29e4d8df9d04b6ca9e8133a
Andrew Maney <andrewmaney05@gmail.com>
2022-07-30 20:49:03 -0400

    Create AudioLowQualityifier.py

---
+++ AudioLowQualityifier.py
@@ -0,0 +1,14 @@
+# Imports
+from pydub import AudioSegment
+
+# Variables
+inf = input("Input file >> ")
+otf = input("Output file >> ")
+bitrate = "8k"
+framerate = 8000
+channels = 1
+
+# DO STUFF AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+sound = AudioSegment.from_file(inf)
+new_sound = sound.set_frame_rate(framerate).set_channels(channels)
+new_sound.export(otf, format="mp3", bitrate=bitrate)
